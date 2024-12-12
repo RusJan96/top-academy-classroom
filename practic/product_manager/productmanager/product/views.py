28d687cc87eba6989e59cf18e8d67802deabc4de
RusJan96 <Khasanova109@mail.ru>
2024-12-12 21:05:40 +0700

    продукт
    сделали новый продукт менеджер))

---
+++ practic/product_manager/productmanager/product/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Product
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'product/product_list.html', {'products': products})
+
+def add_product(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        description = request.POST['description']
+        price = request.POST['price']
+        Product.objects.create(name=name, description=description, price=price)
+        return redirect('product_list')
+    return render(request, 'product/add_product.html')
+
+def edit_product(request, id):
+    product = get_object_or_404(Product, id=id)
+    if request.method == 'POST':
+        product.name = request.POST['name']
+        product.description = request.POST['description']
+        product.price = request.POST['price']
+        product.save()
+        return redirect('product_list')
+    return render(request, 'product/edit_product.html', {'product': product})
+
+def delete_product(request, id):
+    product = get_object_or_404(Product, id=id)
+    product.delete()
+    return redirect('product_list')
+
+# Create your views here.
