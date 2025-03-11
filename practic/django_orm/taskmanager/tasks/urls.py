a02a5082ab406dfba67e2dc8e76c62b892378247
RusJan96 <Khasanova109@mail.ru>
2025-03-11 20:46:08 +0700

    новые-старые домашки
    делаю старые дз

---
+++ practic/django_orm/taskmanager/tasks/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from tasks import views
+
+
+urlpatterns = [
+  path('', views.index, name='index'),
+  path('projects/', views.projects, name='projects'),
+  path('performers/', views.performers, name='performers'),
+  path('tasks/', views.tasks, name='tasks'),
+  path('projects/', views.projects, name='projects'),
+  path('project/<int:project_id>', views.project, name='project'),
+]
